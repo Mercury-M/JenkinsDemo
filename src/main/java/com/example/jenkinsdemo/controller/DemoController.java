7af826f1d6d5852a336b06094087d81830862d3f
马德锋 <309160330@qq.com>
2019-06-11 10:47:51 +0800

    init

---
+++ src/main/java/com/example/jenkinsdemo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/demo")
+    public boolean demo(){
+        return true;
+    }
+}
