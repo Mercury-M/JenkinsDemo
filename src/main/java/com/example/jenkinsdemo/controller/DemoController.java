2bab74b21c6dc3f6e5aecb88dee18459ce0c391b
马德锋 <309160330@qq.com>
2019-06-11 13:49:33 +0800

    true - > false

--- src/main/java/com/example/jenkinsdemo/controller/DemoController.java
+++ src/main/java/com/example/jenkinsdemo/controller/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/demo")
     public boolean demo(){
-        return true;
+        return false;
     }
 }
